4b3515714bd231a20440cd90ad6e32cac5657257
Faisal <ngomongaja17@gmail.com>
2025-04-15 12:31:33 +0700

    repaktoring, hilangkan sementara fitur acak operasi

--- script.js
+++ script.js
@@ -1,161 +1,144 @@
 $(document).ready(function() {
     $('#openSidebar').click(function() {
-        const icon = $(this).find('i');
+        const ikon = $(this).find('i');
         const sidebar = $('#sidebar');
-        
-        if(icon.hasClass('bi-list')) {
-            icon.removeClass('bi-list').addClass('bi-x-lg');
-            sidebar.removeClass('translate-x-full');
-        } else {
-            icon.removeClass('bi-x-lg').addClass('bi-list');
-            sidebar.addClass('translate-x-full');
-        }
+        ikon.toggleClass('bi-list bi-x-lg');
+        sidebar.toggleClass('translate-x-full');
     });
+    buatSoal();
+});
+
+$('select').on('change', function() {
+    const ikon = $('#openSidebar').find('i');
+    const sidebar = $('#sidebar');
+    ikon.toggleClass('bi-list bi-x-lg');
+    sidebar.toggleClass('translate-x-full');
 });
 
 // Konstanta dan cache elemen DOM untuk performa
-let MAX_NUMBER = 1000; // Default value
+let MAKSIMAL_ANGKA = 1000; // Default value
 const $soal = $('.soal');
 const $jawaban = $('#jawaban');
 const $statusBenar = $('#status-jawaban-benar');
 const $statusSalah = $('#status-jawaban-salah');
-const $cekButton = $('#cek');
+const $tombolCek = $('#cek');
 
-// Load saved MAX_NUMBER from localStorage
+// Load saved MAKSIMAL_ANGKA from localStorage
 if (localStorage.getItem('maxNumber')) {
-    MAX_NUMBER = parseInt(localStorage.getItem('maxNumber'));
+    MAKSIMAL_ANGKA = parseInt(localStorage.getItem('maxNumber'));
     $('#max-number').val(localStorage.getItem('maxNumberOption'));
 }
 
-// Update MAX_NUMBER based on select value
+// Update MAKSIMAL_ANGKA based on select value
 $('#max-number').change(function() {
-    const selectedValue = $(this).val();
-    switch(selectedValue) {
+    const nilaiTerpilih = $(this).val();
+    switch(nilaiTerpilih) {
         case 'satuan':
-            MAX_NUMBER = 10;
+            MAKSIMAL_ANGKA = 10;
             break;
         case 'puluhan':
-            MAX_NUMBER = 100;
+            MAKSIMAL_ANGKA = 100;
             break;
         case 'ratusaan':
-            MAX_NUMBER = 1000;
+            MAKSIMAL_ANGKA = 1000;
             break;
         case 'ribuan':
-            MAX_NUMBER = 1000000;
+            MAKSIMAL_ANGKA = 1000000;
             break;
     }
     // Save to localStorage
-    localStorage.setItem('maxNumber', MAX_NUMBER);
-    localStorage.setItem('maxNumberOption', selectedValue);
-    
-    // Generate new question with new MAX_NUMBER
-    generateQuestion();
+    localStorage.setItem('maxNumber', MAKSIMAL_ANGKA);
+    localStorage.setItem('maxNumberOption', $(this).val());
+    buatSoal();
 });
 
 // State aplikasi
 const state = {
     angka1: 0,
     angka2: 0,
-    isCorrect: false,
+    jawabanBenar: false,
     operasi: 'acak' // Default value
 };
 
 // Load saved operasi from localStorage
 if (localStorage.getItem('operasi')) {
-    state.operasi = localStorage.getItem('operasi');
+    state.operasi = localStorage.getItem('operasi'); 
     $('#operasi').val(state.operasi);
 }
 
 // Update operasi based on select value
 $('#operasi').change(function() {
     state.operasi = $(this).val();
-    // Save to localStorage
     localStorage.setItem('operasi', state.operasi);
-    
-    // Generate new question with new operasi
-    generateQuestion();
+    buatSoal();
 });
 
-// Generate angka random 1-MAX_NUMBER
-const getRandomNumber = () => Math.floor(Math.random() * MAX_NUMBER) + 1;
+// Generate angka random 1-MAKSIMAL_ANGKA
+const dapatAngkaAcak = () => Math.floor(Math.random() * MAKSIMAL_ANGKA) + 1;
 
 // Fungsi untuk memperbarui tampilan soal
-const updateSoalDisplay = () => {
-    let soalText = `${state.angka1} + ${state.angka2} ?`;
-    if (state.operasi === 'pengurangan') {
-        soalText = `${state.angka1} - ${state.angka2} ?`;
-    } else if (state.operasi === 'acak') {
-        const randomOperator = Math.random() < 0.5 ? '+' : '-';
-        soalText = `${state.angka1} ${randomOperator} ${state.angka2} ?`;
-    }
-    $soal.text(soalText);
+const perbaruiTampilanSoal = () => {
+    $soal.text(`${state.angka1} ${state.operasi === 'pengurangan' ? '-' : '+'} ${state.angka2} ?`);
 };
 
 // Reset status jawaban
-const resetStatus = () => {
+const aturUlangStatus = () => {
     $statusBenar.addClass('hidden');
     $statusSalah.addClass('hidden');
 };
 
 // Tampilkan status jawaban
-const showStatus = (isCorrect) => {
-    $statusBenar.toggleClass('hidden', !isCorrect);
-    $statusSalah.toggleClass('hidden', isCorrect);
+const tampilkanStatus = (benar) => {
+    $statusBenar.toggleClass('hidden', !benar);
+    $statusSalah.toggleClass('hidden', benar);
 };
 
 // Generate soal baru
-const generateQuestion = () => {
-    state.angka1 = getRandomNumber();
-    state.angka2 = getRandomNumber();
-    state.isCorrect = false;
+const buatSoal = () => {
+    state.angka1 = dapatAngkaAcak();
+    state.angka2 = dapatAngkaAcak();
+    state.jawabanBenar = false; 
     
-    updateSoalDisplay();
+    perbaruiTampilanSoal();
     $jawaban.val('');
-    resetStatus();
+    aturUlangStatus();
 };
 
 // Cek jawaban
-const checkAnswer = () => {
-    const userAnswer = parseInt($jawaban.val()) || 0;
-    let correctAnswer = state.angka1 + state.angka2;
+const periksaJawaban = () => {
+    const jawabanPengguna = parseInt($jawaban.val()) || 0;
+    let jawabanBenar = state.angka1 + state.angka2;
     if (state.operasi === 'pengurangan') {
-        correctAnswer = state.angka1 - state.angka2;
-    } else if (state.operasi === 'acak') {
-        const randomOperator = Math.random() < 0.5 ? '+' : '-';
-        if (randomOperator === '-') {
-            correctAnswer = state.angka1 - state.angka2;
-        }
+        jawabanBenar = state.angka1 - state.angka2;
     }
     
-    state.isCorrect = (userAnswer === correctAnswer);
-    showStatus(state.isCorrect);
+    state.jawabanBenar = (jawabanPengguna === jawabanBenar);
+    tampilkanStatus(state.jawabanBenar);
 };
 
-// Event handlers 
-$(document).ready(generateQuestion);
-$('#buat-soal').click(generateQuestion);
+$('#buat-soal').click(buatSoal);
 
 // Load saved sistem penilayan from localStorage
 if (localStorage.getItem('sistemPenilayan')) {
     const sistemPenilayan = localStorage.getItem('sistemPenilayan');
     $('#sistem-penilayan').val(sistemPenilayan);
     if (sistemPenilayan === 'otomatis') {
-        $cekButton.addClass('hidden');
-        $jawaban.on('input', checkAnswer);
+        $tombolCek.addClass('hidden');
+        $jawaban.on('input', periksaJawaban);
     }
 }
 
 // Update sistem penilayan based on select value
 $('#sistem-penilayan').change(function() {
-    const selectedValue = $(this).val();
-    // Save to localStorage
-    localStorage.setItem('sistemPenilayan', selectedValue);
+    const nilaiTerpilih = $(this).val();
+    localStorage.setItem('sistemPenilayan', nilaiTerpilih);
     
-    if (selectedValue === 'otomatis') {
-        $cekButton.addClass('hidden');
-        $jawaban.on('input', checkAnswer);
+    if (nilaiTerpilih === 'otomatis') {
+        $tombolCek.addClass('hidden');
+        $jawaban.on('input', periksaJawaban);
     } else {
-        $cekButton.removeClass('hidden');
+        $tombolCek.removeClass('hidden');
         $jawaban.off('input');
+        $tombolCek.on('click', periksaJawaban);
     }
 });
